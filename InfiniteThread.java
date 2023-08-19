fe61aea96dabad8417a2f0c77c508f638b7f01e5
Plietnov Vladyslav <pletniov@ukr.net>
2023-08-19 16:53:17 +0200

    third commit

--- InfiniteThread.java
+++ InfiniteThread.java
@@ -3,16 +3,31 @@ public class InfiniteThread {
         Thread t1 = new Thread(){
             public void run(){
                 while(true){
-                    System.out.println("Минуло 5 секунд");
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+                    System.out.println("Минуло 5 секунд");
+                }
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                int count = 1;
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(count);
+                    count++;
                 }
             }
         };
 
         t1.start();
+        t2.start();
     }
 }
